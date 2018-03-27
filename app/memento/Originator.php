45a10d88658f0c7092de6b29168d3c1fff943075
dılo <berxudar@gmail.com>
2018-03-28 00:50:43 +0300

    write test memento class

--- app/memento/Originator.php
+++ app/memento/Originator.php
@@ -6,26 +6,46 @@
 
 namespace App\memento;
 
+/**
+ * Class Originator
+ * @package App\memento
+ */
 class Originator
 {
 
+    /**
+     * @var
+     */
     private $state;
 
+    /**
+     * @param $state
+     */
     public function setState($state)
     {
         $this->state = $state;
     }
 
+    /**
+     * @return mixed
+     */
     public function getState()
     {
         return $this->state;
     }
 
+    /**
+     * @return Memento
+     */
     public function saveStateToMemento()
     {
         return new Memento($this->state);
     }
 
+    /**
+     * @param $memento
+     * @return mixed
+     */
     public function getStateFromMemento($memento)
     {
        return  $this->state = $memento->getState();
