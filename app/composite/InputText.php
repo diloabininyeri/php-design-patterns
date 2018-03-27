05900b46f81795db911441272df67fa39c6c7688
dılo <berxudar@gmail.com>
2018-03-28 01:20:10 +0300

    composite completed and coded testCase

---
+++ app/composite/InputText.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mehmet
+ * Date: 28.03.2018
+ * Time: 00:56
+ */
+
+class InputText
+{
+
+}
---
+++ test/CompositeTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mehmet
+ * Date: 28.03.2018
+ * Time: 01:01
+ */
+
+class CompositeTest
+{
+
+}
