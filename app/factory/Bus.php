da9f38e4234304e349cfd04341dc419df1f98825
dılo <berxudar@gmail.com>
2018-03-27 15:51:56 +0300

    factory pattern added

---
+++ app/factory/Bus.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mehmet
+ * Date: 27.03.2018
+ * Time: 15:26
+ */
+
+namespace App\factory;
+
+
+class Bus
+{
+
+}
---
+++ app/factory/Car.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mehmet
+ * Date: 27.03.2018
+ * Time: 15:24
+ */
+
+class Car
+{
+
+}
