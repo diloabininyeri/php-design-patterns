32cd526a709ac3b42c797538e364d0616909b9ad
dılo <berxudar@gmail.com>
2018-03-30 01:14:34 +0300

    some files deleted

---
+++ app/User.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mehmet
+ * Date: 30.03.2018
+ * Time: 01:09
+ */
+namespace App;
+
+class User
+{
+
+    function deneme()
+    {
+
+        return "merhaba";
+    }
+}
