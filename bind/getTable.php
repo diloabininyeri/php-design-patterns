b315b8160ee6ac885ac1f74da176537b6a4f5574
dılo <berxudar@gmail.com>
2018-03-30 01:22:29 +0300

    optimized code some wheere

--- bind/getTable.php
+++ bind/getTable.php
@@ -1,14 +1,17 @@
 <?php
 /**
+ * in php modern php framework acces protected variable
+ * this example for php framework
+ * also u can reflection class
  * Date: 30.03.2018
  * Time: 00:25
  */
 
 require_once realpath(__DIR__ . "../../vendor/autoload.php");
 
-(function () {
+echo (function () {
 
-    echo $this->table;
+    return  $this->table;
 
 })->call(new Bind\call\Users);
 
